379d1ba282f6c7764f1b270fc2b42d69ceeb2104
sina <sina.moradi2003@yahoo.com>
2023-08-02 21:46:14 +0330

    feat: Adding validate route

--- backend/api/initializers/syncDatabase.go
+++ backend/api/initializers/syncDatabase.go
@@ -7,16 +7,4 @@ func SyncDataBase() {
 	if err != nil {
 		panic("Error in migrating User")
 	}
-	err = DB.AutoMigrate(&models.Customer{})
-	if err != nil {
-		panic("Error in migrating Customer")
-	}
-	err = DB.AutoMigrate(&models.Peer{})
-	if err != nil {
-		panic("Error in migrating Peer")
-	}
-	err = DB.AutoMigrate(&models.System{})
-	if err != nil {
-		panic("Error in migrating System")
-	}
 }
--- backend/api/main.go
+++ backend/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"api/controllers"
 	"api/initializers"
+	"api/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,6 +17,7 @@ func main() {
 	r := gin.Default()
 	r.POST("/signup", controllers.SignUp)
 	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
 
 	r.Run()
 }
