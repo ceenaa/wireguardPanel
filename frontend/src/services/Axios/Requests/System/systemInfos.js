0006f685052338524c587ca158668ed267ab1a85
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-09-04 14:52:21 +0330

    fix: fix merge conflict

--- frontend/src/services/Axios/Requests/System/systemInfos.js
+++ frontend/src/services/Axios/Requests/System/systemInfos.js
@@ -3,7 +3,7 @@ import axiosInstance from '../../Configs/configs';
 
 // GET system infos
 const getSystemInfos = async () =>
-	axiosInstance.get(`/systems/wg0?page=1&per_page=10`, {
+	axiosInstance.get(`/systems/MAHDI?page=1&per_page=10`, {
 		withCredentials: true
 	});
 
--- frontend/src/services/Axios/Requests/System/systemReload.js
+++ frontend/src/services/Axios/Requests/System/systemReload.js
@@ -3,7 +3,7 @@ import axiosInstance from '../../Configs/configs';
 
 // POST new peer
 const postSystemReload = async () =>
-	axiosInstance.post(`/test/systems/wg0/reload`, {
+	axiosInstance.post(`/test/systems/MAHDI/reload`, {
 		withCredentials: true
 	});
 
